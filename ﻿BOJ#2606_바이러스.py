4c9096a728c58c82d18d170499cc50eb778ee2ca
hellokena <42730820+hellokena@users.noreply.github.com>
2021-04-14 04:00:59 +0900

    4/13

---
+++ ﻿BOJ#2606_바이러스.py
@@ -0,0 +1,23 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+e = int(sys.stdin.readline().rstrip())
+graph = [[0]*(n+1) for _ in range(n+1)]
+
+for _ in range(e):
+    from_n, to_n = map(int, sys.stdin.readline().rstrip().split())
+    graph[from_n][to_n] = graph[to_n][from_n] = 1
+
+visited = [0]*(n+1)
+answer = 0
+
+def solution(s): #dfs
+    global answer
+    visited[s] = 1
+    #print(s, end= ' ')
+    answer += 1
+    for i in range(1, n+1):
+        if visited[i]==0 and graph[s][i]==1: solution(i)
+
+solution(1)
+print(answer-1) # 1번 컴퓨터 제외
