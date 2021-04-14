303c3ffbbb99dda7b4309d2ac44de5357f78ee92
hellokena <42730820+hellokena@users.noreply.github.com>
2021-04-14 18:39:36 +0900

    4/13

--- ﻿BOJ#2606_바이러스.py
+++ ﻿BOJ#2606_바이러스.py
@@ -11,13 +11,13 @@ for _ in range(e):
 visited = [0]*(n+1)
 answer = 0
 
-def solution(s): #dfs
+def dfs(s): #dfs
     global answer
     visited[s] = 1
     #print(s, end= ' ')
     answer += 1
     for i in range(1, n+1):
-        if visited[i]==0 and graph[s][i]==1: solution(i)
+        if visited[i]==0 and graph[s][i]==1: dfs(i)
 
-solution(1)
+dfs(1)
 print(answer-1) # 1번 컴퓨터 제외
